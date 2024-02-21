ef1d476e5c7888e351a2060ce0f7d6ba17ffc2d5
xcpvo <160745487+xcpvo@users.noreply.github.com>
2024-02-21 22:50:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+bot = Bot(token="BOT_TOKEN")
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+@dp.chat_join_request_handler()
+async def start1(update: types.ChatJoinRequest):
+    # we accept the user into the channel
+    await update.approve()
+    # the ability to send a message to the user
+    #await bot.send_message(chat_id=update.from_user.id, text="текст сообщения бота в лс")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
